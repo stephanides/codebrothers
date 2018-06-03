9d200d2da7d2da41bcdf4205f5ea104f472921cd
ViktorVojtek <vvojtek@enli.sk>
2018-06-03 19:46:49 +0200

    Removing deprecated functions

--- src/public/js/codebrothers.js
+++ src/public/js/codebrothers.js
@@ -3,10 +3,9 @@ var activeText = 1;
 
 $(document).ready(function() {
   var typingTexts = document.querySelectorAll(".typingText");
-  var svgImgs = document.querySelectorAll(".svg");
-  var navLinks = document.querySelectorAll("a"); // #wrapper header nav a
+  var navLinks = document.querySelectorAll("a");
 
-  $('.lazy').lazy();
+  $(".lazy").lazy();
   
   for(var i = 0; i < typingTexts.length; i++) {
     texts.push(typingTexts[i]);
@@ -14,38 +13,19 @@ $(document).ready(function() {
 
   [].forEach.call(navLinks, function(navLink) {
     navLink.addEventListener("click", function(e) {
-      var sectionNum = parseInt(e.target.classList.item(0).split("-")[1]);
+      var el = e.target;
+      var sectionNum = 0;
+      
+      for(var i = 0; i < el.classList.length; i++) {
+        if(el.classList[i].indexOf("section-") > -1) {
+          sectionNum = parseInt(el.classList[i].split("-")[1]);
+        }
+      }
       
       if(sectionNum) {
         section(sectionNum);
       }
     }, false);
-  });
-
-  [].forEach.call(svgImgs, function(img) {
-    var _img = img;
-    var url = img.src;
- 
-    myFetch(url, function(data) {
-      var parser = new DOMParser();
-      var xmlDoc = parser.parseFromString(data, "text/xml");
-      var svg = xmlDoc.getElementsByTagName("svg")[0];
-      
-      if(_img.id) {
-        svg.setAttribute("id", _img.id);
-      }
-      if(_img.className) {
-        svg.setAttribute("class", _img.className + " replaced-svg");
-      }
-      
-      for(var k = 0; k < svg.attributes.length; k++) {
-        if (svg.attributes[k].name.indexOf("xmlns") > -1) {
-          svg.removeAttribute(svg.attributes[k].name);
-        }
-      }
-      
-      _img.parentElement.replaceChild(svg, _img);
-    });
   });
 
   window.setInterval(function() {
@@ -61,28 +41,14 @@ $(document).ready(function() {
 
 function changeText(param) {
   for(var i = 0; i < texts.length; i++) {
-    texts[i].classList.remove('active');
+    texts[i].classList.remove("active");
   }
   
-  texts[param].classList.add('active');
-}
-
-function myFetch(url, callback) {
-  var req = new XMLHttpRequest();
-  
-  req.addEventListener("load", reqListener);
-  req.open("GET", url);
-  req.send();
-
-  function reqListener () {
-    if(typeof callback === "function") {
-      callback(this.responseText);
-    }
-  }
+  texts[param].classList.add("active");
 }
 
 function section(e) {
-  $('html, body').animate({
+  $("html, body").animate({
     scrollTop: $("section.section-" + e).position().top - $("header").height()
-  }, 'slow');
+  }, "slow");
 }
