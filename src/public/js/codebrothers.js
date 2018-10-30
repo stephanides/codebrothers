e0c6eed3a6630a3456a3364907b7d461916aee37
Viktor Vojtek <vvojtek@enli.sk>
2018-10-30 17:38:24 +0100

    Added sitemap. Replacing setInterval and timeout for requestanimation frame

--- src/public/js/codebrothers.js
+++ src/public/js/codebrothers.js
@@ -96,15 +96,18 @@ function handleLink(el) {
 
 function handleModalOff() {
   modal.classList.replace("show", "hide");
-  setTimeout(function() {
+  window.requestTimeout(function () {
     modal.classList.remove("z-modal");
   }, 300);
+  /*setTimeout(function() {
+    modal.classList.remove("z-modal");
+  }, 300);*/
 }
 
 function startTextTyping() {
   typingTexts[activeText].classList.add("active");
 
-  textTypingInterval = window.setInterval(function() {
+  textTypingInterval = window.requestInterval(function () {
     activeText++;
     
     if (activeText === 3) {
@@ -112,11 +115,20 @@ function startTextTyping() {
     }
 
     changeText(activeText);
-  }, 8000);
+  }, 8000); /*window.setInterval(function() {
+    activeText++;
+    
+    if (activeText === 3) {
+      activeText = 0;
+    }
+
+    changeText(activeText);
+  }, 8000);*/
 }
 
 function stopTextTyping() {
-  window.clearInterval(textTypingInterval);
+  window.clearRequestInterval(textTypingInterval);
+  // window.clearInterval(textTypingInterval);
   textTypingInterval = 0;
   
   if(typingTexts[activeText].classList.contains("active")) {
@@ -159,3 +171,105 @@ function submitForm(e) {
     }
   });
 }
+
+
+// requestAnimationFrame() shim by Paul Irish
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimFrame = (function() {
+	return  window.requestAnimationFrame || 
+    window.webkitRequestAnimationFrame || 
+    window.mozRequestAnimationFrame    || 
+    window.oRequestAnimationFrame      || 
+    window.msRequestAnimationFrame     || 
+    function(/* function */ callback, /* DOMElement */ element){
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+/**
+ * Behaves the same as setInterval except uses requestAnimationFrame() where possible for better performance
+ * @param {function} fn The callback function
+ * @param {int} delay The delay in milliseconds
+ */
+window.requestInterval = function(fn, delay) {
+	if( !window.requestAnimationFrame       && 
+		!window.webkitRequestAnimationFrame && 
+		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+		!window.oRequestAnimationFrame      && 
+		!window.msRequestAnimationFrame)
+			return window.setInterval(fn, delay);
+			
+	var start = new Date().getTime(),
+		handle = new Object();
+		
+	function loop() {
+		var current = new Date().getTime(),
+			delta = current - start;
+			
+		if(delta >= delay) {
+			fn.call();
+			start = new Date().getTime();
+		}
+
+		handle.value = requestAnimFrame(loop);
+	};
+	
+	handle.value = requestAnimFrame(loop);
+	return handle;
+}
+
+/**
+ * Behaves the same as clearInterval except uses cancelRequestAnimationFrame() where possible for better performance
+ * @param {int|object} fn The callback function
+ */
+    window.clearRequestInterval = function(handle) {
+    window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
+    window.webkitCancelAnimationFrame ? window.webkitCancelAnimationFrame(handle.value) :
+    window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value) : /* Support for legacy API */
+    window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
+    window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
+    window.msCancelRequestAnimationFrame ? window.msCancelRequestAnimationFrame(handle.value) :
+    clearInterval(handle);
+};
+
+/**
+ * Behaves the same as setTimeout except uses requestAnimationFrame() where possible for better performance
+ * @param {function} fn The callback function
+ * @param {int} delay The delay in milliseconds
+ */
+
+window.requestTimeout = function(fn, delay) {
+	if( !window.requestAnimationFrame      	&& 
+		!window.webkitRequestAnimationFrame && 
+		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+		!window.oRequestAnimationFrame      && 
+		!window.msRequestAnimationFrame)
+			return window.setTimeout(fn, delay);
+			
+	var start = new Date().getTime(),
+		handle = new Object();
+		
+	function loop(){
+		var current = new Date().getTime(),
+			delta = current - start;
+			
+		delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
+	};
+	
+	handle.value = requestAnimFrame(loop);
+	return handle;
+};
+
+/**
+ * Behaves the same as clearTimeout except uses cancelRequestAnimationFrame() where possible for better performance
+ * @param {int|object} fn The callback function
+ */
+window.clearRequestTimeout = function(handle) {
+    window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
+    window.webkitCancelAnimationFrame ? window.webkitCancelAnimationFrame(handle.value) :
+    window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value) : /* Support for legacy API */
+    window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
+    window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
+    window.msCancelRequestAnimationFrame ? window.msCancelRequestAnimationFrame(handle.value) :
+    clearTimeout(handle);
+};
