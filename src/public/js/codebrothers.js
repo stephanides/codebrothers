183dd7328b9abb769ed893b5728a0e86d4f9c559
Viktor Vojtek <vvojtek@enli.sk>
2018-10-31 13:12:34 +0100

    Switch mixpanel over Google Analytics

--- src/public/js/codebrothers.js
+++ src/public/js/codebrothers.js
@@ -10,10 +10,16 @@ var mobileNav = document.querySelector(".mobile");
 var mobileNavBtn = mobileNav.querySelector(".nav-btn button");
 var mobileNavLinks = mobileNav.querySelectorAll("nav ul li a");
 
-// $(document).ready(function() {});
+// timer variables
+var timer = 0;
+var start = true;     // flags that you want the countdown to start
+var stopIn = 8000;    // how long the timer should run
+var stopTime = 0;     // used to hold the stop time
+var stop = false;     // flag to indicate that stop time has been reached
+var timeTillStop = 0; // holds the display time
 
 window.onload = function () {
-  mixpanel.track("Page Visit");
+  // mixpanel.track("Page Visit");
 
   for(var i = 0; i < typingTexts.length; i++) {
     texts.push(typingTexts[i]);
@@ -107,15 +113,7 @@ function handleModalOff() {
 function startTextTyping() {
   typingTexts[activeText].classList.add("active");
 
-  textTypingInterval = window.requestInterval(function () {
-    activeText++;
-    
-    if (activeText === 3) {
-      activeText = 0;
-    }
-
-    changeText(activeText);
-  }, 8000); /*window.setInterval(function() {
+  /*textTypingInterval = setInterval(function() {
     activeText++;
     
     if (activeText === 3) {
@@ -124,11 +122,51 @@ function startTextTyping() {
 
     changeText(activeText);
   }, 8000);*/
+  // var now = new Date().getTime();
+  // textTypingInterval = draw(now);
+
+  // main update function
+  function update(timer) {
+    timer = timer;
+    if (start) {
+      stopTime = timer + stopIn;
+      
+      activeText++;
+    
+      if (activeText === 3) {
+        activeText = 0;
+      }
+
+      changeText(activeText);
+
+      start = false;
+    } else {
+      if(timer >= stopTime) {
+        stop = true;
+      }
+    }
+
+    timeTillStop = stopTime - timer;
+
+    if(!stop){
+      requestAnimationFrame(update); // continue animation until stop
+    } else {
+      start = true;
+      stop = false;
+      stopTime = 0;
+      timeTillStop = 0;
+      textTypingInterval = requestAnimationFrame(update);
+    }
+  }
+  textTypingInterval = requestAnimationFrame(update);  // start the animation
 }
 
 function stopTextTyping() {
-  window.clearRequestInterval(textTypingInterval);
-  // window.clearInterval(textTypingInterval);
+  start = true;
+  stop = true;
+  stopTime = timer + stopIn;
+  timeTillStop = stopTime - timer;
+  cancelAnimationFrame(textTypingInterval);
   textTypingInterval = 0;
   
   if(typingTexts[activeText].classList.contains("active")) {
@@ -171,105 +209,3 @@ function submitForm(e) {
     }
   });
 }
-
-
-// requestAnimationFrame() shim by Paul Irish
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-window.requestAnimFrame = (function() {
-	return  window.requestAnimationFrame || 
-    window.webkitRequestAnimationFrame || 
-    window.mozRequestAnimationFrame    || 
-    window.oRequestAnimationFrame      || 
-    window.msRequestAnimationFrame     || 
-    function(/* function */ callback, /* DOMElement */ element){
-      window.setTimeout(callback, 1000 / 60);
-    };
-})();
-
-/**
- * Behaves the same as setInterval except uses requestAnimationFrame() where possible for better performance
- * @param {function} fn The callback function
- * @param {int} delay The delay in milliseconds
- */
-window.requestInterval = function(fn, delay) {
-	if( !window.requestAnimationFrame       && 
-		!window.webkitRequestAnimationFrame && 
-		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
-		!window.oRequestAnimationFrame      && 
-		!window.msRequestAnimationFrame)
-			return window.setInterval(fn, delay);
-			
-	var start = new Date().getTime(),
-		handle = new Object();
-		
-	function loop() {
-		var current = new Date().getTime(),
-			delta = current - start;
-			
-		if(delta >= delay) {
-			fn.call();
-			start = new Date().getTime();
-		}
-
-		handle.value = requestAnimFrame(loop);
-	};
-	
-	handle.value = requestAnimFrame(loop);
-	return handle;
-}
-
-/**
- * Behaves the same as clearInterval except uses cancelRequestAnimationFrame() where possible for better performance
- * @param {int|object} fn The callback function
- */
-    window.clearRequestInterval = function(handle) {
-    window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
-    window.webkitCancelAnimationFrame ? window.webkitCancelAnimationFrame(handle.value) :
-    window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value) : /* Support for legacy API */
-    window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
-    window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
-    window.msCancelRequestAnimationFrame ? window.msCancelRequestAnimationFrame(handle.value) :
-    clearInterval(handle);
-};
-
-/**
- * Behaves the same as setTimeout except uses requestAnimationFrame() where possible for better performance
- * @param {function} fn The callback function
- * @param {int} delay The delay in milliseconds
- */
-
-window.requestTimeout = function(fn, delay) {
-	if( !window.requestAnimationFrame      	&& 
-		!window.webkitRequestAnimationFrame && 
-		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
-		!window.oRequestAnimationFrame      && 
-		!window.msRequestAnimationFrame)
-			return window.setTimeout(fn, delay);
-			
-	var start = new Date().getTime(),
-		handle = new Object();
-		
-	function loop(){
-		var current = new Date().getTime(),
-			delta = current - start;
-			
-		delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
-	};
-	
-	handle.value = requestAnimFrame(loop);
-	return handle;
-};
-
-/**
- * Behaves the same as clearTimeout except uses cancelRequestAnimationFrame() where possible for better performance
- * @param {int|object} fn The callback function
- */
-window.clearRequestTimeout = function(handle) {
-    window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
-    window.webkitCancelAnimationFrame ? window.webkitCancelAnimationFrame(handle.value) :
-    window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value) : /* Support for legacy API */
-    window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
-    window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
-    window.msCancelRequestAnimationFrame ? window.msCancelRequestAnimationFrame(handle.value) :
-    clearTimeout(handle);
-};
